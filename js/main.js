5dbf75db248c0da77a4286ecc6d87fcff0f1101c
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-11-13 09:48:02 +0100

    Refactored functionality

--- js/main.js
+++ js/main.js
@@ -2,26 +2,33 @@ const menuBtn = document.querySelector('#menuBtn');
 const menuBtnIcon = menuBtn.querySelector('img');
 const navLinks = document.querySelector('.nav-links');
 
-const openNavbar = () => {
+const openMenu = () => {
+    menuBtn.classList.toggle('open');
+    changeIcon();
+    
     navLinks.classList.toggle('open');
 }
 
-menuBtn.addEventListener('click', openNavbar);
-
-
-
-
-
 const changeIcon = () => {
-    if (menuBtn.classList.contains('closed')) {
-        menuBtn.classList.remove('closed');
-        menuBtn.classList.add('open');
-        menuBtnIcon.setAttribute('src', './assets/images/icon-menu-close.svg');
-    } else {
-        menuBtn.classList.add('closed');
-        menuBtn.classList.remove('open');
-        menuBtnIcon.setAttribute('src', './assets/images/icon-menu.svg');
-    }
+    if (menuBtn.classList.contains('open')) {
+        return menuBtnIcon.setAttribute('src', './assets/images/icon-menu-close.svg');
+    } 
+    return menuBtnIcon.setAttribute('src', './assets/images/icon-menu.svg');
 }
 
-menuBtn.addEventListener('click', changeIcon);
+menuBtn.addEventListener('click', openMenu);
+
+// const changeIcon = () => {
+//     if (menuBtn.classList.contains('closed')) {
+//         menuBtn.classList.remove('closed');
+//         menuBtn.classList.add('open');
+//         menuBtnIcon.setAttribute('src', './assets/images/icon-menu-close.svg');
+//     } else {
+//         menuBtn.classList.add('closed');
+//         menuBtn.classList.remove('open');
+//         menuBtnIcon.setAttribute('src', './assets/images/icon-menu.svg');
+//     }
+// }
+
+
+// menuBtn.addEventListener('click', changeIcon);
