6cb92d7097139032d7d86636e519f118c42b4be2
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-11-13 09:25:27 +0100

    Functionality done, needs rework

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
 const menuBtn = document.querySelector('#menuBtn');
+const menuBtnIcon = menuBtn.querySelector('img');
 const navLinks = document.querySelector('.nav-links');
 
 const openNavbar = () => {
     navLinks.classList.toggle('open');
-    // menuBtn.querySelector('img').setAttribute('src', './assets/images/icon-menu-close.svg');
 }
 
 menuBtn.addEventListener('click', openNavbar);
+
+
+
+
+
+const changeIcon = () => {
+    if (menuBtn.classList.contains('closed')) {
+        menuBtn.classList.remove('closed');
+        menuBtn.classList.add('open');
+        menuBtnIcon.setAttribute('src', './assets/images/icon-menu-close.svg');
+    } else {
+        menuBtn.classList.add('closed');
+        menuBtn.classList.remove('open');
+        menuBtnIcon.setAttribute('src', './assets/images/icon-menu.svg');
+    }
+}
+
+menuBtn.addEventListener('click', changeIcon);
