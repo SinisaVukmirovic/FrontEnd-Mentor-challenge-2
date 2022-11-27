082e59711e60a1db54c6b5bf84b2650e1d94fa67
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2022-11-27 15:41:30 +0100

    Refactored to use picture elem for image on different screensize

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 import { bodyElem } from './DOMElems.js';
-import openMenu from "./openMenu.js";
+import "./openMenu.js";
 
-let headlineImg = bodyElem.querySelector('.headline figure img');
+// let headlineImg = bodyElem.querySelector('.headline figure img');
 
-const headlineImages = {
-    mobile: './assets/images/image-web-3-mobile.jpg',
-    desktop: './assets/images/image-web-3-desktop.jpg'
-}
+// const headlineImages = {
+//     mobile: './assets/images/image-web-3-mobile.jpg',
+//     desktop: './assets/images/image-web-3-desktop.jpg'
+// }
 
-if (window.innerWidth >= 1440) {
-    headlineImg.src = headlineImages.desktop;
-}
+// if (window.innerWidth >= 1440) {
+//     headlineImg.src = headlineImages.desktop;
+// }
